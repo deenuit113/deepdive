be81c17d71329bc2d4db671f998bd2c6b9c79e86
deenuit113 <leejh5314@gmail.com>
2024-08-23 14:32:52 +0900

    240823 mission

---
+++ practice/240823/index.js
@@ -0,0 +1,38 @@
+let A = 1;
+let B = 2;
+
+// 덧셈
+let add = A + B;
+console.log("덧셈 결과: " + add);
+
+// 뺄셈
+let subtract = A - B;
+console.log("뺄셈 결과: " + subtract);
+
+// 곱하기
+let multiply = A * B;
+console.log("곱셈 결과: " + multiply);
+
+// 나누기
+let divide = A / B;
+console.log("나눗셈 결과: " + divide);
+
+// 나머지
+let remainder = A % B;
+console.log("나머지 결과: " + remainder);
+
+// 전위 증가
+let preIncrement = ++A;
+console.log("A: " + A + ", 전위 증가 연산 결과: " + preIncrement);
+
+// 전위 감소
+let preDecrement = --A;
+console.log("A: " + A + ", 전위 증가 연산 결과: " + preDecrement);
+
+// 후위 증가
+let postIncrement = A++;
+console.log("A: " + A + ", 전위 증가 연산 결과: " + preIncrement);
+
+// 후위 감소
+let postDecrement = A--;
+console.log("A: " + A + ", 전위 증가 연산 결과: " + preDecrement);
---
+++ practice/240823/index1.js
@@ -0,0 +1,27 @@
+let A = 1;
+let B = 2;
+
+
+// 비교 연산자 ==
+let isEqual = A == B;
+console.log("값이 같은가요? " + isEqual);
+
+// 비교 연산자 !=
+let isNotEqual = A != B;
+console.log("다른가요? " + isNotEqual);
+
+// 비교 연산자 ===
+let isStrictEqual = A === B;
+console.log("타입과 값이 같은가요? " + isStrictEqual);
+
+// 비교 연산자 !==
+let isStrictNotEqual = A !== B;
+console.log("타입과 값이 다른가요?" + isStrictNotEqual);
+
+// 비교 연산자 >
+let isGreaterThan = A > B;
+console.log("A가 더 큰가요? " + isGreaterThan);
+
+// 비교 연산자 >=
+let isGreaterThanOrEqual = A >= B;
+console.log("A가 크거나 같나요? " + isGreaterThanOrEqual);
